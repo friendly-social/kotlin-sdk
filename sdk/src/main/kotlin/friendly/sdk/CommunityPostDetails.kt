663984adff89ebdd8301139deb6e7c665617d2ad
y9san9 / Alex Sokol <y9san9@gmail.com>
2026-08-13 13:04:15 +0300

    feat(community): added /details endpoint to get full info about community

--- sdk/src/main/kotlin/friendly/sdk/CommunityPostDetails.kt
+++ sdk/src/main/kotlin/friendly/sdk/CommunityPostDetails.kt
@@ -2,7 +2,7 @@ package friendly.sdk
 
 import kotlin.time.Instant
 
-public data class CommunityPost(
+public data class CommunityPostDetails(
     val id: CommunityPostId,
     val accessHash: CommunityPostAccessHash,
     val text: CommunityPostText,
@@ -13,8 +13,8 @@ public data class CommunityPost(
     public val descriptor: CommunityPostDescriptor =
         CommunityPostDescriptor(id, accessHash)
 
-    public fun serializable(): CommunityPostSerializable =
-        CommunityPostSerializable(
+    public fun serializable(): CommunityPostDetailsSerializable =
+        CommunityPostDetailsSerializable(
             id = id.serializable(),
             accessHash = accessHash.serializable(),
             text = text.serializable(),
