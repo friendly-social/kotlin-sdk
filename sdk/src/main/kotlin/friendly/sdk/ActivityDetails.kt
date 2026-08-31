ee1c91d78dd6a4348aa3a9b1b66231bf9737930f
y9san9 / Alex Sokol <y9san9@gmail.com>
2026-08-31 22:10:23 +0300

    feat(activity): added read status

--- sdk/src/main/kotlin/friendly/sdk/ActivityDetails.kt
+++ sdk/src/main/kotlin/friendly/sdk/ActivityDetails.kt
@@ -5,18 +5,21 @@ import kotlin.time.Instant
 public sealed interface ActivityDetails {
     public val id: ActivityId
     public val instant: Instant
+    public val isRead: Boolean
 
     public fun serializable(): ActivityDetailsSerializable
 
     public data class Reply(
         override val id: ActivityId,
         override val instant: Instant,
+        override val isRead: Boolean,
         val post: CommunityPostDetails.Plain,
     ) : ActivityDetails {
         override fun serializable(): ActivityDetailsSerializable =
             ActivityDetailsSerializable.Reply(
                 id = id.serializable(),
                 instant = instant,
+                isRead = isRead,
                 post = post.serializable(),
             )
     }
